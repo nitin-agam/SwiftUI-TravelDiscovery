7076bf85b7bb1aaacd83fd3b71714bcf85dfb26b
nitin-agam <nitinagam17@gmail.com>
2022-12-22 17:53:14 +0530

    feat: Initial setup with launch screen

---
+++ Travel Discovery/Travel Discovery/Travel_DiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_DiscoveryApp.swift
+//  Travel Discovery
+//
+//  Created by Nitin Aggarwal on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Travel_DiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
