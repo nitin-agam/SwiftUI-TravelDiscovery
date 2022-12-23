fee0d9509e7316b32a4523afb15a09c9eb0b506e
nitin-agam <nitinagam17@gmail.com>
2022-12-23 13:40:00 +0530

    feat: UI setup for discover categories [WIP]

--- Travel Discovery/Travel Discovery/Home/HomeView.swift
+++ Travel Discovery/Travel Discovery/Home/HomeView.swift
@@ -15,6 +15,14 @@ struct HomeView: View {
                 // MARK: - Discover Categories View
                 DiscoverCategoriesView()
                 
+                // MARK: - Popular Destinations View
+                PopularDestinationsView()
+             
+                // MARK: - Popular Places View
+                PopularPlacesView()
+                
+                // MARK: - Trending Creators View
+                TrendingCreatorsView()
             }
             .navigationTitle("Discover")
         }
@@ -23,23 +31,131 @@ struct HomeView: View {
 
 struct DiscoverCategoriesView: View {
     
+    let categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live Events", imageName: "music.mic"),
+        .init(name: "Food", imageName: "fork.knife.circle.fill"),
+        .init(name: "History", imageName: "music.mic")
+    ]
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 12) {
-                ForEach(0..<15) { _ in
+            HStack(alignment: .top, spacing: 14) {
+                ForEach(categories) { category in
                     VStack(spacing: 8) {
-                        Spacer()
+                        Image(systemName: category.imageName)
+                            .font(.system(size: 25))
+                            .foregroundColor(.white)
                             .frame(width: 60, height: 60)
                             .background(Color.gray)
                             .cornerRadius(.infinity)
-                            .shadow(color: .gray, radius: 5, x: 0, y: 2)
+                            .shadow(color: .gray, radius: 3, x: 0, y: 2)
                         
-                        Text("Category")
-                            .font(.system(size: 15, weight: .medium))
+                        Text(category.name)
+                            .font(.system(size: 13, weight: .medium))
+                            .multilineTextAlignment(.center)
                     }
+                    .frame(width: 72)
                 }
             }
             .padding(.horizontal)
+        }
+    }
+}
+
+struct PopularDestinationsView: View {
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular destinations")
+                    .font(.system(size: 17, weight: .semibold))
+                
+                Spacer()
+                
+                Text("See all")
+                    .font(.system(size: 13, weight: .medium))
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(0..<15) { _ in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(Color.gray)
+                            .cornerRadius(10)
+                            .shadow(color: .gray, radius: 3, x: 0, y: 2)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct PopularPlacesView: View {
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 17, weight: .semibold))
+                
+                Spacer()
+                
+                Text("See all")
+                    .font(.system(size: 13, weight: .medium))
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(0..<15) { _ in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(10)
+                            .shadow(color: .gray, radius: 3, x: 0, y: 2)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct TrendingCreatorsView: View {
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trending creators")
+                    .font(.system(size: 17, weight: .semibold))
+                
+                Spacer()
+                
+                Text("See all")
+                    .font(.system(size: 13, weight: .medium))
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(0..<15) { _ in
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 3, x: 0, y: 2)
+                    }
+                }
+                .padding(.horizontal)
+            }
         }
     }
 }
---
+++ Travel Discovery/Travel Discovery/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  Travel Discovery
+//
+//  Created by Nitin Aggarwal on 23/12/22.
+//
+
+import Foundation
+
+struct Category: Identifiable {
+    let id = UUID()
+    let name, imageName: String
+}
