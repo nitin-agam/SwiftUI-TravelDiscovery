7f902e7d3facc0d721ef56ddb2ddd72da33c06c8
nitin-agam <nitinagam17@gmail.com>
2022-12-23 16:53:46 +0530

    feat: UI setup for popular destination

--- Travel Discovery/Travel Discovery/Home/HomeView.swift
+++ Travel Discovery/Travel Discovery/Home/HomeView.swift
@@ -17,7 +17,7 @@ struct HomeView: View {
                 
                 // MARK: - Popular Destinations View
                 PopularDestinationsView()
-             
+                
                 // MARK: - Popular Places View
                 PopularPlacesView()
                 
@@ -66,6 +66,18 @@ struct DiscoverCategoriesView: View {
 
 struct PopularDestinationsView: View {
     
+    let destinations: [Destination] = [
+        Destination(name: "Bagan", country: "Myanmar", imageName: "Bagan, Myanmar"),
+        Destination(name: "Banff Park", country: "Canada", imageName: "Banff National Park, Canada"),
+        Destination(name: "Bora Bora", country: "France", imageName: "Bora Bora, French Polynesia"),
+        Destination(name: "Forbid City", country: "China", imageName: "Forbidden City, Beijing, China"),
+        Destination(name: "Great Ocean", country: "Australia", imageName: "Great Ocean Road, Australia"),
+        Destination(name: "Cherry Blossom", country: "Japan", imageName: "Japan in Cherry Blossom Season"),
+        Destination(name: "Pamukkale", country: "Turkey", imageName: "Pamukkale, Turkey"),
+        Destination(name: "Venice", country: "Italy", imageName: "Venice, Italy"),
+        Destination(name: "Danxia Geopark", country: "China", imageName: "Zhangye Danxia Geopark, China")
+    ]
+    
     var body: some View {
         VStack {
             HStack {
@@ -82,12 +94,48 @@ struct PopularDestinationsView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 12) {
-                    ForEach(0..<15) { _ in
-                        Spacer()
-                            .frame(width: 125, height: 150)
-                            .background(Color.gray)
-                            .cornerRadius(10)
-                            .shadow(color: .gray, radius: 3, x: 0, y: 2)
+                    ForEach(destinations) { destination in
+                        
+                        ZStack() {
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 150, height: 180)
+                            
+                            Rectangle()
+                                .fill(
+                                    .linearGradient(Gradient(colors: [
+                                        Color.black.opacity(0),
+                                        Color.black.opacity(0),
+                                        Color.black.opacity(0),
+                                        Color.black.opacity(0),
+                                        Color.black.opacity(0.4),
+                                        Color.black.opacity(0.6)
+                                    ]), startPoint: .top, endPoint: .bottom)
+                                )
+                            
+                            VStack(alignment: .leading, spacing: 2) {
+                                
+                                HStack {
+                                    Spacer()
+                                }
+
+                                Spacer()
+                                
+                                Text(destination.name)
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 17, weight: .semibold))
+                                    .padding(.top)
+                                Text(destination.country)
+                                    .foregroundColor(.white.opacity(0.8))
+                                    .font(.system(size: 14, weight: .medium))
+                            }
+                            .frame(width: 132)
+                            .padding(.bottom, 6)
+                        }
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                        .shadow(color: .gray, radius: 3, x: 0, y: 2)
                     }
                 }
                 .padding(.horizontal)
---
+++ Travel Discovery/Travel Discovery/Model/Destination.swift
@@ -0,0 +1,13 @@
+//
+//  Destination.swift
+//  Travel Discovery
+//
+//  Created by Nitin Aggarwal on 23/12/22.
+//
+
+import Foundation
+
+struct Destination: Identifiable {
+    let id = UUID()
+    let name, country, imageName: String
+}
